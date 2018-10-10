8069bb783d1a011039a8743a7671d75457fa27bd
李卓原 <zhuoyuan93@gmail.com>
2018-10-10 18:11:35 +0800

    不让futureBuilder瞎重绘

--- lib/FutureBuilderPage.dart
+++ lib/FutureBuilderPage.dart
@@ -1,7 +1,7 @@
 /*
  * Created by 李卓原 on 2018/9/30.
  * email: zhuoyuan93@gmail.com
- *
+ * 关于状态改变引起的不必要的页面刷新：https://github.com/flutter/flutter/issues/11426#issuecomment-414047398
  */
 
 import 'dart:async';
@@ -15,6 +15,9 @@ class FutureBuilderPage extends StatefulWidget {
 }
 
 class FutureBuilderState extends State<FutureBuilderPage> {
+  String title = 'FutureBuilder使用';
+  var _futureBuilderFuture;
+
   Future _gerData() async {
     var response = HttpUtil()
         .get('http://api.douban.com/v2/movie/top250', data: {'count': 15});
@@ -22,16 +25,34 @@ class FutureBuilderState extends State<FutureBuilderPage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    ///用_futureBuilderFuture来保存_gerData()的结果，以避免不必要的ui重绘
+    _futureBuilderFuture = _gerData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FutureBuilder使用'),
+        title: Text(title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            title = title + '.';
+          });
+        },
+        child: Icon(Icons.title),
       ),
       body: RefreshIndicator(
         onRefresh: _gerData,
         child: FutureBuilder(
           builder: _buildFuture,
-          future: _gerData(), // 用户定义的需要异步执行的代码，类型为Future<String>或者null的变量或函数
+          future:
+              _futureBuilderFuture, // 用户定义的需要异步执行的代码，类型为Future<String>或者null的变量或函数
         ),
       ),
     );
@@ -41,16 +62,22 @@ class FutureBuilderState extends State<FutureBuilderPage> {
   Widget _buildFuture(BuildContext context, AsyncSnapshot snapshot) {
     switch (snapshot.connectionState) {
       case ConnectionState.none:
+        print('还没有开始网络请求');
         return Text('还没有开始网络请求');
       case ConnectionState.active:
+        print('active');
         return Text('ConnectionState.active');
       case ConnectionState.waiting:
+        print('waiting');
         return Center(
           child: CircularProgressIndicator(),
         );
       case ConnectionState.done:
+        print('done');
         if (snapshot.hasError) return Text('Error: ${snapshot.error}');
         return _createListView(context, snapshot);
+      default:
+        return Text('还没有开始网络请求');
     }
   }
 
