3cf263bbcc7a252447ca02bb309bb0e303f04337
李卓原 <zhuoyuan93@gmail.com>
2019-02-01 16:49:41 +0800

    分享

--- lib/share_page.dart
+++ lib/share_page.dart
@@ -15,51 +15,47 @@ class SharePage extends StatefulWidget {
 class _SharePageState extends State<SharePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('share to twitter'),
-              onPressed: () async {
-                var response = await FlutterShareMe().shareToTwitter(
-                    url: 'https://github.com/lizhuoyuan',
-                    msg: 'hello flutter! ');
-                if (response == 'success') {
-                  print('navigate success');
-                }
-              },
-            ),
-            RaisedButton(
-              child: Text('share to shareWhatsApp'),
-              onPressed: () {
-                FlutterShareMe().shareToWhatsApp(
-                    msg:
-                        'hello,this is my github:https://github.com/lizhuoyuan');
-              },
-            ),
-            RaisedButton(
-              child: Text('share to shareFacebook'),
-              onPressed: () {
-                FlutterShareMe().shareToFacebook(
-                    url: 'https://github.com/lizhuoyuan', msg: 'Hello Flutter');
-              },
-            ),
-            RaisedButton(
-              child: Text('share to System'),
-              onPressed: () async {
-                var response =
-                    await FlutterShareMe().shareToSystem(msg: 'Hello Flutter');
-                if (response == 'success') {
-                  print('navigate success');
-                }
-              },
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('share to twitter'),
+            onPressed: () async {
+              var response = await FlutterShareMe().shareToTwitter(
+                  url: 'https://github.com/lizhuoyuan', msg: 'hello flutter! ');
+              if (response == 'success') {
+                print('navigate success');
+              }
+            },
+          ),
+          RaisedButton(
+            child: Text('share to shareWhatsApp'),
+            onPressed: () {
+              FlutterShareMe().shareToWhatsApp(
+                  msg: 'hello,this is my github:https://github.com/lizhuoyuan');
+            },
+          ),
+          RaisedButton(
+            child: Text('share to shareFacebook'),
+            onPressed: () {
+              FlutterShareMe().shareToFacebook(
+                  url: 'https://github.com/lizhuoyuan', msg: 'Hello Flutter');
+            },
+          ),
+          RaisedButton(
+            child: Text('share to System'),
+            onPressed: () async {
+              var response =
+                  await FlutterShareMe().shareToSystem(msg: 'Hello Flutter');
+              if (response == 'success') {
+                print('navigate success');
+              }
+            },
+          ),
+        ],
       ),
     );
   }
