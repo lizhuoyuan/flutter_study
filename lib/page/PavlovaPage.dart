f2e0c2dadac27493f50afab4718e3428e825cee0
LiZhuoyuan <zhuoyuan93@gmail.com>
2020-07-05 22:48:17 +0800

    no message

--- lib/page/PavlovaPage.dart
+++ lib/page/PavlovaPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PavlovaPage extends StatefulWidget {
   @override
