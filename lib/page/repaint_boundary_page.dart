d24f602630007f2f97e5435b573396b52d926cd6
LiZhuoyuan <zhuoyuan93@gmail.com>
2020-08-25 00:29:10 +0800

    屏幕截图

--- lib/page/repaint_boundary_page.dart
+++ lib/page/repaint_boundary_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -43,12 +45,30 @@ class _RepaintBoundaryPageState extends State<RepaintBoundaryPage> {
   Future<void> _screenShot() async {
     RenderRepaintBoundary boundary =
         _globalKey.currentContext.findRenderObject();
-    var image = await boundary.toImage();
+    ui.Image image = await boundary.toImage();
     ByteData byteData = await image.toByteData(format: ImageByteFormat.png);
     Uint8List pngBytes = byteData.buffer.asUint8List();
-    print(pngBytes);
+
     setState(() {
       uint8list = pngBytes;
     });
+
+    byte2File(pngBytes);
   }
+
+  /// 把图片ByteData写入File
+  Future<void> byte2File(Uint8List pngBytes) async {
+    Directory tempDir = await getTemporaryDirectory();
+    String storagePath = tempDir.path;
+
+    File file = new File('$storagePath/截图.png');
+
+    if (!file.existsSync()) {
+      file.createSync();
+    }
+    file.writeAsBytesSync(pngBytes);
+  }
+
+  //path_provider 提供的方法
+  Future getTemporaryDirectory() {}
 }
