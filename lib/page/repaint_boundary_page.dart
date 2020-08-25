cbd12d1ead0ac055292f538825149dde09c9bd92
LiZhuoyuan <zhuoyuan93@gmail.com>
2020-08-26 01:10:34 +0800

    文件读取,截图保存

--- lib/page/repaint_boundary_page.dart
+++ lib/page/repaint_boundary_page.dart
@@ -5,6 +5,8 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:path_provider/path_provider.dart';
 
 class RepaintBoundaryPage extends StatefulWidget {
   @override
@@ -14,14 +16,26 @@ class RepaintBoundaryPage extends StatefulWidget {
 class _RepaintBoundaryPageState extends State<RepaintBoundaryPage> {
   GlobalKey _globalKey = GlobalKey();
   Uint8List uint8list;
+  File file;
+
+  @override
+  void initState() {
+    super.initState();
+    //getFile();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.delete_forever),
+          onPressed: deleteFile,
+        ),
         appBar: AppBar(
           title: Text('屏幕/组件截图'),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             RaisedButton(
               child: Text('以下的东西会截图'),
@@ -33,13 +47,25 @@ class _RepaintBoundaryPageState extends State<RepaintBoundaryPage> {
                 children: [
                   Text('图里的文字'),
                   Icon(Icons.accessibility),
+                  Expanded(child: TextField()),
                 ],
               ),
             ),
-            Text('以下为截图的内容'),
+            Text('↓以下为截图的内容↓'),
             if (uint8list != null) Image.memory(uint8list),
+            Text('↓下面的图片为本地保存的图片文件↓'),
+            if (file?.existsSync() == true) Image.file(file),
           ],
         ));
+  }
+
+  Future<void> deleteFile() async {
+    bool fileIsExist = await file.exists();
+    if (fileIsExist) {
+      file.deleteSync();
+      setState(() {});
+      print('删除图片成功:${file.existsSync()}');
+    }
   }
 
   Future<void> _screenShot() async {
@@ -58,17 +84,22 @@ class _RepaintBoundaryPageState extends State<RepaintBoundaryPage> {
 
   /// 把图片ByteData写入File
   Future<void> byte2File(Uint8List pngBytes) async {
-    Directory tempDir = await getTemporaryDirectory();
-    String storagePath = tempDir.path;
-
-    File file = new File('$storagePath/截图.png');
-
+    await getFile();
     if (!file.existsSync()) {
       file.createSync();
+      print('生成图片文件:$file');
     }
     file.writeAsBytesSync(pngBytes);
+    if (file.existsSync()) {
+      print('图片保存成功');
+      Fluttertoast.showToast(msg: '图片保存成功');
+    }
   }
 
-  //path_provider 提供的方法
-  Future getTemporaryDirectory() {}
+  Future<void> getFile() async {
+    Directory tempDir = await getTemporaryDirectory();
+    file = File('${tempDir.path}/img${DateTime.now()}.png');
+
+    setState(() {});
+  }
 }
